b2ccab0feaadd24679675b8c3208ec6c994f6ed2
afkarahmad <137616979+afkarahmad@users.noreply.github.com>
2023-07-08 12:44:06 +0530

    Index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+       <title>
+       </title>
+       <meta name="viewport"content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet"href="#">
+        <link rel="icon"href="#">
+
+
+
+        <style>
+
+            /*My CSS texts*/
+
+        </style>
+
+    </head>
+     <body>
+
+
+          <!--My HTML body is here-->
+<h1>hello everyone,it is my PHP</h1>
+<?php 
+   echo"hello guise kesse ho"
+   ?>
+
+
+         <script>
+              //my javascript
+
+
+
+
+         </script>
+
+
+
+
+
+
+
+    </body>
+
+
+</html>
